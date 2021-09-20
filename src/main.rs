f7fc2d5987ab2ed49e69a08f9b9c2a35f93787b2
isirasan <c.giessler@mailbox.org>
2021-09-20 22:19:55 +0200

    minor fix

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use std::env;
 
 fn main() {
     let arguments: Vec<String> = env::args().collect();
-    let result = roll::execute(&arguments);
+    let result = roll::roll::execute(&arguments);
     print!("Total: {}\n\n", result);
 }
